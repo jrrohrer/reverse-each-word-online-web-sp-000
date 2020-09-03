a59d8f5808e793eccda77b3dce7c2ec72b4ddce3
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-03 17:57:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   original_array = string.split(" ")
   return_array = []
-  original_array.each do |word|
+  original_array.collect do |word|
     return_array << word.reverse
   end
   return_array.join(" ")
