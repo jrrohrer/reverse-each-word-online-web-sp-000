2cec1f3cefcc8f1dbf9b22234bcd0a6a28e191d7
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-03 17:55:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,3 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
-#step 1: turn the sentence string into an array
-#step 2: reverse the individual strings and store this as a new array
-#step 3: turn the new array back into a single string and output the reversed string
