ff2e3ec0013ad2660082edb40557c487e5e8e3d3
Jessica Rohrer <jessie.rohrer@gmail.com>
2020-09-03 17:38:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,11 @@
 def reverse_each_word(sentence)
-  string_array = sentence.join(",")
+  string_array = sentence.split(",")
   reversed_words = []
   string_array.each do |word|
     reversed_words.push(word.reverse)
   end
-  reversed_words.
+  reversed_sentence = reversed_words.join(" ")
+  puts reversed_sentence
 end
 
 #step 1: turn the sentence string into an array
